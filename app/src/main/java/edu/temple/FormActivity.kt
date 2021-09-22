a5511109c42ee83c53e2117bbcbde2ed77cc606a
Liam <Liam@DESKTOP-BBOI67G>
2021-09-22 09:52:17 -0400

    Fixed input bugs and added error catching and displaying the toast for all correct information

--- app/src/main/java/edu/temple/FormActivity.kt
+++ app/src/main/java/edu/temple/FormActivity.kt
@@ -4,18 +4,17 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 
 class FormActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
-
         findViewById<Button>(R.id.buttonRegister).setOnClickListener {
             val username = findViewById<TextView>(R.id.inputUsername)
             val email = findViewById<TextView>(R.id.inputEmail)
-            val password = findViewById<TextView>(R.id.inputEmail)
+            val password = findViewById<TextView>(R.id.inputPassword)
             val passwordConfirm = findViewById<TextView>(R.id.inputPasswordConfirm)
             var passwordMatch = true
             var valid = true
@@ -38,19 +37,21 @@ class FormActivity : AppCompatActivity() {
                 if(password.text.toString() == passwordConfirm.text.toString()){
 
                 }else{
+                    password.error = "Passwords do not match"
+                    passwordConfirm.error = "Passwords do not match"
                     passwordMatch = false
                     valid = false
                 }
             } else{
-                username.error = "Password is a required field"
+                password.error = "Password is a required field"
+                passwordConfirm.error = "Password is a required field"
                 valid = false
                 passwordMatch = false
             }
 
-
-
+            if(valid && passwordMatch){
+                Toast.makeText(applicationContext, "Hello, " + username.text.toString() + ", to the SignUpFormActivity", Toast.LENGTH_SHORT).show()
+            }
         }
-
-
     }
 }
