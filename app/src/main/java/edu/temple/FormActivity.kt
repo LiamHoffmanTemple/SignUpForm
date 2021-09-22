2ed6e4845a3734d8f3ca24479bbfe7d442f39153
Liam <Liam@DESKTOP-BBOI67G>
2021-09-22 09:27:25 -0400

    Created UI and added basic input logic

--- app/src/main/java/edu/temple/FormActivity.kt
+++ app/src/main/java/edu/temple/FormActivity.kt
@@ -2,10 +2,55 @@ package edu.temple
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class FormActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+        findViewById<Button>(R.id.buttonRegister).setOnClickListener {
+            val username = findViewById<TextView>(R.id.inputUsername)
+            val email = findViewById<TextView>(R.id.inputEmail)
+            val password = findViewById<TextView>(R.id.inputEmail)
+            val passwordConfirm = findViewById<TextView>(R.id.inputPasswordConfirm)
+            var passwordMatch = true
+            var valid = true
+
+            if (username.text.toString().isNotEmpty()){
+
+            } else{
+                username.error = "Username is a required field"
+                valid = false
+            }
+
+            if (email.text.toString().isNotEmpty()){
+
+            } else{
+                email.error = "Email is a required field"
+                valid = false
+            }
+
+            if (password.text.toString().isNotEmpty() && passwordConfirm.text.toString().isNotEmpty()){
+                if(password.text.toString() == passwordConfirm.text.toString()){
+
+                }else{
+                    passwordMatch = false
+                    valid = false
+                }
+            } else{
+                username.error = "Password is a required field"
+                valid = false
+                passwordMatch = false
+            }
+
+
+
+        }
+
+
     }
 }
